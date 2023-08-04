d6c1fee4174aaca61175c56cfd45fea4592fb4e3
RAHIME KIR <RAHIME.KIR@lcwaikiki.com>
2023-08-04 11:59:53 +0300

    Add project files.

---
+++ RabbitMq.Publisher/Program.cs
@@ -0,0 +1,68 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Linq;
+using System.Text;
+
+namespace RabbitMq.Publisher
+{
+
+    public enum LogNames
+    {
+        Critical = 1,
+        Error = 2,
+        Warning = 3,
+        Info = 4
+
+    }
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory();
+            factory.Uri = new Uri("amqps://kfsdoqic:jn0RezxxesWFhHedSXKfrxlcieC0PsC_@fish.rmq.cloudamqp.com/kfsdoqic");
+
+            using var connection = factory.CreateConnection();
+
+            var channel = connection.CreateModel();
+
+
+            channel.ExchangeDeclare("logs-direct", type: ExchangeType.Direct, durable: true);  //=>Exchange oluşturma işlemi
+
+           
+            Enum.GetNames(typeof(LogNames)).ToList().ForEach(x =>
+            {
+                //her enum için kuyruk oluşturma işlemi
+                var queueName = $"direct-queue - {x}" ;
+                
+                channel.QueueDeclare(queueName, true, false, false);
+                //routeKey belirleme
+                var routeKey = $"route-{x}";
+
+                channel.QueueBind(queueName,"logs-direct", routeKey,null); // routekeylerin ilgili  kuyruğa set edilmesi için 
+
+            });
+
+            
+            Enumerable.Range(1, 50).ToList().ForEach(x =>
+            {
+                LogNames log = (LogNames)new Random().Next(1, 5);
+
+                string message = $"log-type {log} ";
+                var messageBody = Encoding.UTF8.GetBytes(message);
+
+                //routeKey belirleme
+                var routeKey = $"route-{log}";
+
+
+                //channel.BasicPublish(string.Empty, string routingkey, null, messageBody); => eskisi
+                channel.BasicPublish("logs-direct", routeKey, null, messageBody);  //eskisinde direkt kuyruğa gönderdiğimiz içi  kuyruğun ismini vermiştik burda boş kalacak.
+
+                Console.WriteLine($"Log gönderilmiştir.: {message}");
+            }
+            );
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ RabbitMq.Subscriber/Program.cs
@@ -0,0 +1,47 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+using System.Threading;
+
+namespace RabbitMq.Subscriber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory();
+            factory.Uri = new Uri("amqps://kfsdoqic:jn0RezxxesWFhHedSXKfrxlcieC0PsC_@fish.rmq.cloudamqp.com/kfsdoqic");
+            using var connection = factory.CreateConnection();
+
+            var channel = connection.CreateModel();
+        
+    
+            channel.BasicQos(0, 1, false);
+            var subscriber = new EventingBasicConsumer(channel);
+
+            var queueName = "direct-queue - Error";
+            Console.WriteLine("Loglar dinleniyor ..");
+
+
+            subscriber.Received += (object sender, BasicDeliverEventArgs e) =>
+            {
+                var message = Encoding.UTF8.GetString(e.Body.ToArray());
+
+                Thread.Sleep(1500);
+                Console.WriteLine("Gelen mesaj : " + message);
+
+                channel.BasicAck(e.DeliveryTag, false);
+            };
+
+            channel.BasicConsume(queueName, false, subscriber);
+
+            Console.ReadLine();
+        }
+
+
+    }
+}
+
+
+
